c81af58c57dfd662964a7f38b2129df50ffc084c
Manuel Ortega <manuel@lasindias.coop>
2018-03-29 18:28:43 -0400

    Added domain attribute to Account model

--- db/migrate/20180329191952_create_accounts.rb
+++ db/migrate/20180329191952_create_accounts.rb
@@ -2,6 +2,7 @@ class CreateAccounts < ActiveRecord::Migration[5.1]
   def change
     create_table :accounts do |t|
       t.string :username, null: false, default: ''
+      t.string :domain
       t.timestamps
     end
   end
